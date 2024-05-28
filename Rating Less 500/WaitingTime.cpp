b07f3d32da2eabf9fa3bb73acb92cbbf84c53618
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-05-28 08:16:27 +0530

    4 problems added on 28-05-2024

--- Rating Less 500/WaitingTime.cpp
+++ Rating Less 500/WaitingTime.cpp
@@ -1,5 +1,6 @@
 // Chef is eagerly waiting for a piece of information. His secret agent told him that this information would be revealed to him after K weeks. X days have already passed and Chef is getting restless now. Find the number of remaining days Chef has to wait for, to get the information. It is guaranteed that the information has not been revealed to the Chef yet.
 
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -7,11 +8,13 @@ int main(){
     cout<<"Enter the no. of test cases : ";
     cin>>testCase;
     while(testCase--){
-        int weeksChefHave,passedDays;
-        cout<<"Enter the no. of weeks chef have : ";
-        cin>>weeksChefHave;
-        cout<<"Enter the no. of days chef already passed : ";
-        cin>>passedDays;
-        cout<<"The number of remaining days Chef has to wait will be : "<<((weeksChefHave * 7) - passedDays)<<endl;
+        int reveledWeeks,noOfDaysPassed;
+        cout<<"Enter the no. of weeks to get reveled : ";
+        cin>>reveledWeeks;
+
+        cout<<"Enter the no. of days passed already : ";
+        cin>>noOfDaysPassed;
+
+        cout<<"No. of remaining days Chef has to wait will be : "<<((7 * reveledWeeks) - noOfDaysPassed)<<endl;
     }
 }
--- Rating Less 500/WaterRequirement.cpp
+++ Rating Less 500/WaterRequirement.cpp
@@ -1,5 +1,6 @@
 // Finally, after purchasing a water cooler during the April long challenge, Chef noticed that his water cooler requires 2 liters of water to cool for one hour.How much water (in liters) would be required by the cooler to cool for N hours?
 
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -7,9 +8,9 @@ int main(){
     cout<<"Enter the no. of test cases : ";
     cin>>testCase;
     while(testCase--){
-        int hours;
+        int noOfHours;
         cout<<"Enter the no. of hours : ";
-        cin>>hours;
-        cout<<"Water would be required : "<<(2 * hours)<<endl;
+        cin>>noOfHours;
+        cout<<"No. of water needed : "<<(2 * noOfHours)<<" liter"<<endl;
     }
 }
